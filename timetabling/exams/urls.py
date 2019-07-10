273aee0fe9ac25db158c768b78408fd2a60aaf05
Will Bolton <willyjayb@gmail.com>
2019-07-10 23:40:17 +0100

    Implement basic inbuilt authorisation
    This doesn't quite work yet, since the admin templates seem to clash
    with the default templates used in the out-of-the-box views, and seem to
    be taking precedence.
    
    Tried a few things to remedy this, including reordering the installed
    apps in the settings, but it's been a very long day and I'm too tired to
    try anything else.
    
    Will look at the LOGIN_REDIRECT_URL and its two pals later too.

--- timetabling/exams/urls.py
+++ timetabling/exams/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from exams import views
 
 urlpatterns = [
-    path('/', views.something),
+    path('', views.index),
 ]
--- timetabling/exams/views.py
+++ timetabling/exams/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    return(HttpResponse("Hiya! You're logged in as %s."%request.user))
